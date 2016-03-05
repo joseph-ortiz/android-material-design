487be1df2e57e86b3062bb067a80e583b9356f50
jortiz <josepho011235@gmail.com>
2016-03-04 23:07:31 -0500

    Added custom color palettes

--- app/src/main/java/com/example/jortiz/androidmaterialdesign/MainActivity.java
+++ app/src/main/java/com/example/jortiz/androidmaterialdesign/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jortiz.androidmaterialdesign;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // call some material design API's here
+
+        } else { // For below API 21
+            // IMplement this feature without material design
+        }
     }
 }
